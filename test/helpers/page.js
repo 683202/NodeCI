84321965322a50fce6f026759a1ecb268f9845b6
Rajeev Ranjan <rajeev.ranjan.3@team.telstra.com>
2019-03-09 15:01:01 +0530

    Travis config

--- test/helpers/page.js
+++ test/helpers/page.js
@@ -6,8 +6,8 @@ class CustomPage {
 
     static async build() {
         const browser = await puppeteer.launch({ 
-            headless: true
-            // args: [--no-sandbox]
+            headless: true,
+            args: [--no-sandbox]
         });
         const page = await browser.newPage();
         const customPage = new CustomPage(page);
